376205fef4aeec5f5b31eb7239a116b5823746c6
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2016-12-22 23:08:47 +0100

    Day2 - Part 2

--- adventofcode/src/main/kotlin/day2.kt
+++ adventofcode/src/main/kotlin/day2.kt
@@ -2,7 +2,7 @@ import io.kotlintest.specs.StringSpec
 
 /***
  *
- --- Day 2: Bathroom Security ---
+--- Day 2: Bathroom Security ---
 
 You arrive at Easter Bunny Headquarters under cover of darkness.
 However, you left in such a rush that you forgot to use the bathroom!
@@ -40,24 +40,62 @@ So, in this example, the bathroom code is 1985.
 Your puzzle input is the instructions from the document you found at the front desk. What is the bathroom code?
 
 
+--- Part Two ---
+
+You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors can behold the many
+fancy conference rooms and water coolers on this floor) and go to punch in the code.
+
+Much to your bladder's dismay, the keypad is not at all like you imagined it.
+
+Instead, you are confronted with the result of hundreds of man-hours of bathroom-keypad-design meetings:
+
+    1
+  2 3 4
+5 6 7 8 9
+  A B C
+    D
+You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very different:
+
+You start at "5" and don't move at all (up and left are both edges), ending at 5.
+Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
+Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
+Finally, after five more moves, you end at 3.
+So, given the actual keypad layout, the code would be 5DB3.
+
+Using the same instructions in your puzzle input, what is the correct bathroom code?
+
 
 
  */
 class Day2 : StringSpec() { init {
+    "Matrix" {
+        val matrix = KeyPad.simpleKeypad().matrix
+        matrix[1][1] shouldBe 1
+        matrix[1][3] shouldBe 3
+        matrix[3][1] shouldBe 7
+    }
     "Moves" {
+        val pad = KeyPad.simpleKeypad()
+
         val myTable = table(
-            headers("nb", "left", "right", "up", "down"),
-            row(1, 1, 2, 1, 4),
-            row(3, 2, 3, 3, 6),
-            row(4, 4, 5, 1, 7),
-            row(5, 4, 6, 2, 8),
-            row(7, 7, 8, 4, 7)
+            headers("nb", "x", "y", "left", "right", "up", "down"),
+            row(1, 1, 1, 1, 2, 1, 4),
+            row(3, 1, 3, 2, 3, 3, 6),
+            row(4, 2, 1, 4, 5, 1, 7),
+            row(5, 2, 2, 4, 6, 2, 8),
+            row(7, 3, 1, 7, 8, 4, 7)
         )
-        forAll(myTable) { nb, left, right, up, down ->
-            nb.left() shouldBe left
-            nb.right() shouldBe right
-            nb.up() shouldBe up
-            nb.down() shouldBe down
+        forAll(myTable) { nb, x, y, left, right, up, down ->
+            fun testValue(expected: Int, operation: KeyPad.() -> Unit) {
+                pad.row = x
+                pad.col= y
+                pad.operation()
+                pad.value() shouldBe expected
+            }
+            testValue(left, KeyPad::left)
+            testValue(right, KeyPad::right)
+            testValue(up, KeyPad::up)
+            testValue(down, KeyPad::down)
         }
     }
 
@@ -72,9 +110,10 @@ class Day2 : StringSpec() { init {
         )
         forAll(t) { line, expected ->
             digicode.readLine(line)
-            digicode.code.last() shouldBe expected
+            digicode.found.last() shouldBe expected
         }
     }
+
 
     val puzzle = """
     RDLULDLDDRLLLRLRULDRLDDRRRRURLRLDLULDLDLDRULDDLLDRDRUDLLDDRDULLLULLDULRRLDURULDRUULLLUUDURURRDDLDLDRRDDLRURLLDRRRDULDRULURURURURLLRRLUDULDRULLDURRRLLDURDRRUUURDRLLDRURULRUDULRRRRRDLRLLDRRRDLDUUDDDUDLDRUURRLLUDUDDRRLRRDRUUDUUULDUUDLRDLDLLDLLLLRRURDLDUURRLLDLDLLRLLRULDDRLDLUDLDDLRDRRDLULRLLLRUDDURLDLLULRDUUDRRLDUDUDLUURDURRDDLLDRRRLUDULDULDDLLULDDDRRLLDURURURUUURRURRUUDUUURULDLRULRURDLDRDDULDDULLURDDUDDRDRRULRUURRDDRLLUURDRDDRUDLUUDURRRLLRR
@@ -84,56 +123,104 @@ class Day2 : StringSpec() { init {
     ULURLRDLRUDLLDUDDRUUULULUDDDDDRRDRULUDRRUDLRRRLUDLRUULRDDRRLRUDLUDULRULLUURLLRLLLLDRDUURDUUULLRULUUUDRDRDRUULURDULDLRRULUURURDULULDRRURDLRUDLULULULUDLLUURULDLLLRDUDDRRLULUDDRLLLRURDDLDLRLLLRDLDRRUUULRLRDDDDRUDRUULDDRRULLDRRLDDRRUDRLLDUDRRUDDRDLRUDDRDDDRLLRDUULRDRLDUDRLDDLLDDDUUDDRULLDLLDRDRRUDDUUURLLUURDLULUDRUUUDURURLRRDULLDRDDRLRDULRDRURRUDLDDRRRLUDRLRRRRLLDDLLRLDUDUDDRRRUULDRURDLLDLUULDLDLDUUDDULUDUDRRDRLDRDURDUULDURDRRDRRLLRLDLU
     """.trimIndent()
 
-    "Solution: 33444" {
+    val solution1 = "33444"
+    "Simple Keypad : Solution $solution1" {
         val digicode = Digicode.initial()
         for (line in puzzle.lines()) {
             digicode.readLine(line)
         }
-        digicode.code shouldBe listOf(3, 3, 4, 4, 4)
+        digicode.code() shouldBe solution1
+    }
+
+    val solution2 = "446A6"
+    "Brain-fucked keypad example has solution $solution2" {
+        val digicode = Digicode(KeyPad.brainFuck(), mutableListOf())
+        digicode.keypad.value() shouldBe 5
+        for (line in puzzle.lines()) {
+            digicode.readLine(line)
+        }
+        digicode.code() shouldBe solution2
     }
 }
 }
 
-//Test failed for (nb, 4), (left, 3), (right, 5), (up, 1), (down, 7) with error 4 did not equal 3
-private fun Int.left(): Int = ifSameRow(this - 1, this)
-private fun Int.right(): Int = ifSameRow(this + 1, this)
-private fun Int.up(): Int = ifValid(this - 3, this)
-private fun Int.down(): Int = ifValid(this + 3, this)
 
-data class Digicode(var state: Int, val code: MutableList<Int>) {
+data class Digicode(val keypad: KeyPad, val found: MutableList<Int>) {
 
+    fun code(): String = buildString {
+        for (i in found) {
+            if (i in 1..9) {
+                append(i)
+            } else {
+                append(when (i) {
+                    10 -> 'A'
+                    11 -> 'B'
+                    12 -> 'C'
+                    13 -> 'D'
+                    else -> 'Z'
+                })
+            }
+        }
+    }
     fun readLine(line: String) {
         line.forEach { readLetter(it) }
-        code += state
+        found += keypad.value()
     }
+
     fun readLetter(direction: Char) {
-        state = when (direction) {
-            'L' -> state.left()
-            'R' -> state.right()
-            'U' -> state.up()
-            'D' -> state.down()
+        when (direction) {
+            'L' -> keypad.left()
+            'R' -> keypad.right()
+            'U' -> keypad.up()
+            'D' -> keypad.down()
             else -> TODO("Invalid character $direction")
         }
     }
+
     companion object {
-        fun initial() = Digicode(5, mutableListOf())
+        fun initial() = Digicode(KeyPad.simpleKeypad(), mutableListOf())
     }
 }
 
-fun ifSameRow(ok: Int, ko: Int): Int =
-    if (ko.row() == ok.row()) {
-        ok
-    } else {
-        ko
+
+
+
+
+data class KeyPad(var row: Int, var col: Int, val size: Int, val max: Int, val matrix: List<List<Int>>) {
+
+    fun value() = matrix[row][col]
+    fun left() = tryMove(row, col - 1)
+    fun right() = tryMove(row, col + 1)
+    fun up() = tryMove(row - 1, col)
+    fun down() = tryMove(row + 1, col)
+
+    private fun tryMove(xx: Int, yy: Int) {
+        if (matrix[xx][yy] != 0) {
+            row = xx
+            col = yy
+        }
     }
 
-private fun Int.row() = when (this) {
-    in 1..3 -> 1
-    in 4..6 -> 2
-    in 7..9 -> 3
-    else -> 0
+    companion object {
+
+        fun simpleKeypad() = KeyPad(2, 2, 3, 9, listOf(
+            listOf(0, 0, 0, 0, 0),
+            listOf(0, 1, 2, 3, 0),
+            listOf(0, 4, 5, 6, 0),
+            listOf(0, 7, 8, 9, 0),
+            listOf(0, 0, 0, 0, 0)
+        ))
+
+        fun brainFuck() = KeyPad(3, 1, 3, 9, listOf(
+listOf(0 , 0 , 0  , 0  , 0  , 0 , 0) ,
+listOf(0 , 0 , 0  , 1  , 0  , 0 , 0) ,
+listOf(0 , 0 , 2  , 3  , 4  , 0 , 0) ,
+listOf(0 , 5 , 6  , 7  , 8  , 9 , 0) ,
+listOf(0 , 0 , 10 , 11 , 12 , 0 , 0) ,
+listOf(0 , 0 , 0  , 13 , 0  , 0 , 0) ,
+listOf(0 , 0 , 0  , 0  , 0  , 0 , 0)
+        ))
+
+    }
 }
-
-private fun ifValid(ok: Int, ko: Int): Int = if (ok in 1..9) ok else ko
-
 
